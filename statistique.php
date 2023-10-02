c49df7716dca05a300e5cabdcfefa2c2ce5179cc
Alexis-PRAIA <alexis.praiacarranca@lycee-jeanrostand.fr>
2023-10-02 09:45:11 +0200

    Mise à jour des exos

--- statistique.php
+++ statistique.php
@@ -1,5 +1,22 @@
 <?php
-    function mediane ($myTab){
-        sort(myTab);
+    function mediane ($tab){
+
+        sort($tab);
+
+        $longTab = count($tab);
+
+        //Si le tableau est impaire
+
+        if($longTab%2==1){
+            $index = ($longTab+1)/2;
+            echo ($tab[$index-1]);
+        }
+
+        //Si le tableau est paire
+
+        if($longTab%2==0){
+            $index = ($longTab)/2;
+            echo ($tab[$index-1]) + $tab[$index]/2;
+        }
     }
 ?>
